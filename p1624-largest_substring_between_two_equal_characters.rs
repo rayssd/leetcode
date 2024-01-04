6d36d6251f52a3d3fa9b8bdd1e41564d728fe1d4
rayssd <67847901+rayssd@users.noreply.github.com>
2024-01-04 23:38:02 +1100

    First commit

---
+++ p1624-largest_substring_between_two_equal_characters.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn max_length_between_equal_characters(s: String) -> i32 {
+        use std::cmp;
+
+        let mut start;
+        let mut end;
+        let mut longest:i32 = -1;
+
+        
+        let s: Vec<char> = s.chars().collect();
+
+        for i in 0..s.len() {
+            for j in (i..s.len()).rev() {
+                if s[i] == s[j] {
+                    start = i;   
+                    end = j;
+                    longest = cmp::max(longest, end as i32 - start as i32 - 1);
+                }
+            }
+        }
+
+        longest
+        
+    }
+}
---
+++ p6-zigzag_conversion.rs
@@ -0,0 +1,52 @@
+impl Solution {
+    pub fn convert(s: String, num_rows: i32) -> String {
+        if (s.len() <= 1 )| (num_rows <= 1) { return s; }
+
+        enum Direction {
+            Up,
+            Down
+        }
+
+        let num_rows: usize = num_rows as usize;
+
+        let num_rows = num_rows as usize;
+
+        let v: Vec<char> = s.chars().collect();
+        let mut row = 0;
+        let mut column = 0;
+        
+        let mut zigzag = vec![vec![' ';v.len()]; num_rows];
+        
+        let mut direction = Direction::Down;
+
+        for i in 0..v.len() {
+            match direction {
+                Direction::Down => {
+                    zigzag[row][column] = v[i];
+                    row += 1;
+                    if row >= num_rows {
+                        direction = Direction::Up;
+                        row = num_rows - 1; // when reached the bottom, reset row to last row
+                    }
+                }
+                Direction::Up => {
+                    row -= 1;
+                    column += 1;
+                    zigzag[row][column] = v[i];
+                    if row == 0 {
+                        direction = Direction::Down;
+                        row = 1; // when reached the top, next write at row 2
+                    }
+                }
+            }    
+        }
+
+        let mut new_vec = Vec::new();
+        for r in zigzag.iter_mut() {
+            r.retain(|&c| c != ' ');
+            new_vec.append(r);
+        }
+        let new_string: String = new_vec.iter().collect();
+        new_string 
+    }
+}
